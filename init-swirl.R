77198f04d4c30066098c0b897b5e6ac39d81c5d8
Hwai-Jung Hsu <elexy.fw@gmail.com>
2016-10-28 04:41:24 +0800

    init scripts

---
+++ init-swirl.R
@@ -0,0 +1,15 @@
+local({
+  check_package <- function(pkg) {
+    if (!suppressWarnings(suppressMessages(require(pkg, character.only = TRUE)))) {
+      install.packages(pkg, repos = "http://wush978.github.io/R")
+    }
+  }
+  lapply(c("rappdirs", "bitops", "curl"), check_package)
+  utils::install.packages("swirl", repos = "http://wush978.github.io/R")
+})
+library(swirl)
+library(curl)
+library(methods)
+try(uninstall_course("DataScienceAndR"), silent=TRUE)
+try(uninstall_course("DSC2016-R"), silent=TRUE)
+swirl::install_course_github("hjhsu", "RDM_hj2016", "Course")
